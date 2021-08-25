64bca5ba82a5b58d8236e56b96ba3226c54e9b5e
andyngojs <dongngo.2000@gmail.com>
2021-08-25 22:28:34 +0700

    feat: add background music and add question

---
+++ dataQuestion.js
@@ -0,0 +1,74 @@
+const questions = [
+  {
+    question: "What is 1 + 1 = ?",
+    answers: [
+      { text: "A. 2", correct: true },
+      { text: "B. Two", correct: false },
+      { text: "C. 3 - 1", correct: false },
+      { text: "D. 4", correct: false },
+    ],
+  },
+  {
+    question: "Who is the best YouTuber?",
+    answers: [
+      { text: "A. Web Dev Simplified", correct: false },
+      { text: "B. Traversy Media", correct: false },
+      { text: "C. Dev Ed", correct: true },
+      { text: "D. Fun Fun Function", correct: false },
+    ],
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "Vạn lý trường thành nằm ở đâu?",
+    answers: [
+      { text: "A. Nhật Bản", correct: false },
+      { text: "B. Trung Quốc", correct: true },
+      { text: "C. Hàn Quốc", correct: false },
+      { text: "D. Ấn Độ", correct: false },
+    ],
+  },
+  {
+    question: "Ai là the best?",
+    answers: [
+      { text: "A. CM Punk", correct: true },
+      { text: "B. John Cena", correct: false },
+      { text: "C. Roman Reigns", correct: false },
+      { text: "D. Dean Ambrose", correct: false },
+    ],
+  },
+  {
+    question: "Bạn có người yêu chưa?",
+    answers: [
+      { text: "A. Có rồi", correct: false },
+      { text: "B. Đéo có", correct: false },
+      { text: "C. Yêu đương gì tầm này", correct: true },
+      { text: "D. Dean Ambrose", correct: false },
+    ],
+  },
+  {
+    question: "Ai đẹp trai nhất?",
+    answers: [
+      { text: "A. Trần Đức", correct: false },
+      { text: "B. andyngojs", correct: true },
+      { text: "C. Cả A & B", correct: false },
+      { text: "D. Cả 2 đáp án trên đều đúng", correct: false },
+    ],
+  },
+  {
+    question: "Bài hát 'Muộn rồi mà sao còn' là của ai?",
+    answers: [
+      { text: "A. A.C Xuân Tài", correct: false },
+      { text: "B. Sơn Tùng MTP", correct: true },
+      { text: "C. Tùng Sơn", correct: false },
+      { text: "D. Jack", correct: false },
+    ],
+  },
+];
